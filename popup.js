f7021a1b806af8fcdf9af8d498d249437a918b47
Iñaki Aranzadi <iiaranzadi@gmail.com>
2016-02-28 14:25:19 -0800

    first commit

---
+++ popup.js
@@ -0,0 +1,55 @@
+var $tiles = $('#color-tiles');
+var $tabs = $('.tabs');
+var $tabContainer = $('.tabs-container');
+var tilesContainerStyle = 'display: flex; flex-wrap: wrap; justify-content: center;';
+var clipboard = new Clipboard('.btn');
+
+function getContrastYIQ(hexcolor){
+  var hex = hexcolor.slice(1, hexcolor.length);
+	var r = parseInt(hex.substr(0,2),16);
+	var g = parseInt(hex.substr(2,2),16);
+	var b = parseInt(hex.substr(4,2),16);
+	var yiq = ((r*299)+(g*587)+(b*114))/1000;
+	return (yiq >= 200) ? 'black' : 'white';
+}
+
+$.each(colors, function(index, value){
+  var tab = '<li class="tab-link ' + value.id + '-tab" data-tab="' + value.id + '">' + value.title + '</li>';
+  var tabContent = '<div id="' + value.id + '" class="tab-content"></div>';
+  var section = '<h3>'+ value.title +'</h3><div style="' + tilesContainerStyle + '" id="' + value.id + '"></div>';
+
+  $tabs.append(tab);
+  $tabContainer.append(tabContent);
+  $tiles.append(section);
+
+  $.each(value.colorArray, function(i, v){
+    var textColor = getContrastYIQ(v[1]);
+    $('#' + value.id + '')
+      .append('<div class="tile btn" data-clipboard-text="' + v[0].toLowerCase() + '" style="background-color:' + v[0] + '"><div class="name ' + textColor + '">' + v[0] + '</div></div>');
+  });
+});
+
+$('#pinks').addClass('current');
+$('.pinks-tab').addClass('current');
+
+$('ul.tabs li').click(function(){
+	var tab_id = $(this).attr('data-tab');
+
+	$('ul.tabs li').removeClass('current');
+	$('.tab-content').removeClass('current');
+
+	$(this).addClass('current');
+	$("#"+tab_id).addClass('current');
+});
+
+$(".tabs-container").organicTabs({
+    "speed": 200
+});
+
+function copyColor() {
+  chrome.tabs.executeScript({
+    file: 'copycolor.js'
+  });
+}
+
+$('.btn').on('click', copyColor);
